25bb04a61e9d3c2938bf257677c585b428ebc18e
SergeiCherk <user@SeredgaCherkasov>
2025-11-04 12:57:43 +0300

    Fix 500 error on projects and blog pages

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,19 @@ from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published=True)
+    # Преобразуем tags в список
+    for post in posts:
+        if post.tags:
+            post.tag_list = [tag.strip() for tag in post.tags.split(',')]
+        else:
+            post.tag_list = []
+    
     return render(request, 'blog/list.html', {'posts': posts})
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug, published=True)
+    if post.tags:
+        post.tag_list = [tag.strip() for tag in post.tags.split(',')]
+    else:
+        post.tag_list = []
     return render(request, 'blog/detail.html', {'post': post})
--- projects/views.py
+++ projects/views.py
@@ -3,7 +3,20 @@ from .models import Project
 
 def project_list(request):
     projects = Project.objects.all()
+    # Преобразуем tech_stack в список
+    for project in projects:
+        if project.tech_stack:
+            project.tech_list = [tech.strip() for tech in project.tech_stack.split(',')]
+        else:
+            project.tech_list = []
+    
     featured_projects = Project.objects.filter(is_featured=True)
+    for project in featured_projects:
+        if project.tech_stack:
+            project.tech_list = [tech.strip() for tech in project.tech_stack.split(',')]
+        else:
+            project.tech_list = []
+    
     context = {
         'projects': projects,
         'featured_projects': featured_projects,
@@ -12,4 +25,8 @@ def project_list(request):
 
 def project_detail(request, pk):
     project = get_object_or_404(Project, pk=pk)
+    if project.tech_stack:
+        project.tech_list = [tech.strip() for tech in project.tech_stack.split(',')]
+    else:
+        project.tech_list = []
     return render(request, 'projects/detail.html', {'project': project})
